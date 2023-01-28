03b12dbbb812b14aef470c7bda61f7bcbf5e2b6b
Oleg Batanov <legbat@mail.ru>
2023-01-28 20:15:58 +0300

    update src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,14 +4,13 @@ import readlineSync from 'readline-sync';
 import generateRandomInt from '../utils.js';
 import playGame from '../index.js';
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const gameDescription = console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const gameCore = () => {
   const tempArray = [];
   const randomInt = generateRandomInt(1, 101);
   const getAnswer = readlineSync.question(`Question: ${randomInt}\nYour answer: `);
   const evenNumber = randomInt % 2 === 0;
-  //   const oddNumber = randomInt % 2 !== 0;
 
   const getCorrectAnswer = () => {
     let correct;
@@ -27,13 +26,8 @@ const gameCore = () => {
   return tempArray;
 };
 
-const finalArray = gameCore();
-
-const hint = `'${finalArray[0]}' is wrong answer ;(. Correct answer was '${finalArray[1]}'.
-            Let's try again, `;
-
 const start = () => {
-  playGame(gameCore, finalArray[0], finalArray[1], hint);
+  playGame(gameCore, gameDescription);
 };
 
 export default start;
