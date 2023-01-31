5d572dc1931ee0b92415a4c06c7e2f8670444e59
Oleg Batanov <legbat@mail.ru>
2023-01-31 23:50:27 +0300

    optimization of logic

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,29 +5,29 @@ import playGame from '../index.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  let i = 2;
+  while (i < number) {
+    if (number % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+
+  return true;
+};
+
 const startGameCore = () => {
   const answerStore = [];
   const randomInt = generateRandomInt(1, 20);
   const question = `Question: ${randomInt}\nYour answer: `;
 
-  const isPrime = () => {
-    if (randomInt < 2) {
-      return 'no';
-    }
+  const correctAnswer = isPrime(randomInt) ? 'yes' : 'no';
 
-    let i = 2;
-    const range = randomInt;
-    while (i < range) {
-      if (randomInt % i === 0) {
-        return 'no';
-      }
-      i += 1;
-    }
-
-    return 'yes';
-  };
-
-  const correctAnswer = isPrime();
   answerStore.push(question, correctAnswer);
 
   return answerStore;
