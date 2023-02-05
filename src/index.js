2b4278dceeda829a60262d6bdcb8dead2361f0e9
Oleg Batanov <legbat@mail.ru>
2023-02-05 13:19:54 +0300

    src/index.js - greeting and name request inside playgame()

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@
 
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-
 const playGame = (gameCore, gameDescription) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(gameDescription);
 
   for (let i = 0; i < 3; i += 1) {
